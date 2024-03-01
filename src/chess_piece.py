8c218484e9a8a8ca4d25b09e072897e261d59a13
mason Passinault <passinaultm@gmail.com>
2024-03-01 16:23:41 -0500

    Initial commit of Mason branch

--- src/chess_piece.py
+++ src/chess_piece.py
@@ -1,9 +1,18 @@
-class ChessPiece:
-    __player = None
+from abc import ABC, abstractmethod
+from player import Player
 
+class ChessPiece:
+    def __init__(self, player: Player):
+        self.__player = player
+
+    @abstractmethod   
     def __str__(self):
         pass
-    def typer(self):
+
+    @abstractmethod
+    def type(self):
         pass
+
+    @abstractmethod
     def is_valid_move(self, move, board):
         pass
