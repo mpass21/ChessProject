7ec3dcd53784700dddd93fca7a9d3b36861e28f3
synjin <synjinshanley@gmail.com>
2024-03-10 22:06:48 -0400

    fixed validity

--- src/king.py
+++ src/king.py
@@ -17,27 +17,6 @@ class King(ChessPiece):
             return False
         if abs(move.to_col - move.from_col) > 1 or abs(move.to_row - move.from_row) > 1:
             return False
-        if board[move.to_row + 1][move.to_col] == board[move.from_row][move.from_col]:
-            return True
-        if board[move.to_row - 1][move.to_col] == board[move.from_row][move.from_col]:
-            return True
-        if move.to_row + 1 < 8:
-            if move.to_col + 1 < 8:
-                if board[move.to_row + 1][move.to_col + 1] == board[move.from_row][move.from_col]:
-                    return True
-                if board[move.to_row][move.to_col + 1] == board[move.from_row][move.from_col]:
-                    return True
-            if move.to_col - 1 >= 0:
-                if board[move.to_row + 1][move.to_col - 1] == board[move.from_row][move.from_col]:
-                    return True
-                if board[move.to_row][move.to_col - 1] == board[move.from_row][move.from_col]:
-                    return True
-        if move.to_row - 1 >= 0:
-            if move.to_col + 1 < 8:
-                if board[move.to_row - 1][move.to_col + 1] == board[move.from_row][move.from_col]:
-                    return True
-            if move.to_col - 1 >= 0:
-                if board[move.to_row - 1][move.to_col - 1] == board[move.from_row][move.from_col]:
-                    return True
+        return True
 
 
