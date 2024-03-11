b16eca1e2239f15951459eac72ea6709bacbdc24
mason Passinault <passinaultm@gmail.com>
2024-03-10 20:03:29 -0400

    Pawn Class complete

--- src/pawn.py
+++ src/pawn.py
@@ -1,63 +1,60 @@
+
 from chess_piece import ChessPiece
 from move import Move
 from player import Player
 
-
-
 class Pawn(ChessPiece):
-    def __init__(self, player):
-        self.__player = player
+    def __init__(self, player: Player):
+        super().__init__(player)
 
     def __str__(self):
-        return "pawn"
+        return "Pawn"
 
     def type(self):
-        return "pawn"
+        return "Pawn"
 
-    def is_valid_move(self, move, board):
-        #y is row and x is column
-        #starting locations are row 6,0-7
-        start_row, start_col = move.start_row, move.start_col
-        end_row, end_col = move.end_row, move.end_col
-
-
-        #setting the varibles based on color
-        if self.__player == Player.White:
-            beginCol = 6
-            degree = 1
+    def is_valid_move(self, move: Move, board):
+        start_row, start_col = move.from_row, move.from_col
+        end_row, end_col = move.to_row, move.to_col
+        
+        # Setting the variables based on color
+        if self.player == Player.WHITE:
+            direction = -1
+            begin_row = 6
         else:
-            beginCol = 1
-            degree -1
-        
-
-        #for moving forward one move 
-        if start_col == end_col and end_row == start_row + degree:
-            if board[end_row][end_col] is None:
-                return True
+            direction = 1
+            begin_row = 1
+            
+        if move.from_col == move.to_col and move.from_row == move.to_row:
+            return False
+        if not (0 <= start_row < 8 and 0 <= start_col < 8 and 0 <= end_row < 8 and 0 <= end_col < 8):
             return False
 
-        #for starting move of white 
-        if start_row == beginCol and start_col >= 0 and start_col < 8 and end_col == start_col and end_row == start_row + degree*2:
-            if board[end_row][end_col] is None and board[end_row][end_col-1] is None:
-                return True
+        if board[start_row][start_col].player !=self.player:
+            return False
+        if board[start_row][start_col] != self:
             return False
 
-        
-        #for capturing as white
-        if ((end_col == start_col - 1) or (end_col == start_col + 1)) and end_row == start_row + degree:
-            if board[end_row][end_col]:
+        if start_col == end_col and end_row == start_row + direction and board[end_row][end_col] is None:
+            return True
+        if start_row == begin_row and start_col == end_col and end_row == start_row + direction * 2:
+            if board[end_row][end_col] is None and board[end_row - direction][end_col] is None:
                 return True
-            return False
+        if abs(start_col - end_col) == 1 and end_row == start_row + direction:
+            if board[end_row][end_col] is not None and board[end_row][end_col].player != self.player:
+                return True
+        return False
+
+
+
+
+        
+       
         
 
-testFirstRow = 3
-testLastRow = 4
-testfirstCol = 2
-testLastCol = 4
-
-#joe = Player(1)
-#dog = Pawn(joe)
-#move = Move(testFirstRow, testfirstCol, testLastRow, testLastCol)
 
 
-#dog.is_valid_move(move,)
+
+
+
+
