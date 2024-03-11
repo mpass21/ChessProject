2c2769921cbb70b165895949acc9d9fcd1bac7b9
synjin <synjinshanley@gmail.com>
2024-03-10 21:18:47 -0400

    testing validity

--- src/king.py
+++ src/king.py
@@ -11,31 +11,30 @@ class King(ChessPiece):
     def is_valid_move(self, move, board):
         valid = super().is_valid_move(move, board)
         if valid:
-            valid = False
+            pass
         else:
-            return valid
+            return False
         if abs(move.to_col - move.from_col) > 1 or abs(move.to_row - move.from_row) > 1:
             return False
         if move.to_row + 1 < 7:
             if board[move.to_row + 1][move.to_col] == board[move.from_row][move.from_col]:
-                valid = True
+                return True
             if move.to_col + 1 < 7:
                 if board[move.to_row + 1][move.to_col + 1] == board[move.from_row][move.from_col]:
-                    valid = True
+                    return True
                 if board[move.to_row][move.to_col + 1] == board[move.from_row][move.from_col]:
-                    valid = True
+                    return True
             if move.to_col - 1 > -1:
                 if board[move.to_row + 1][move.to_col - 1] == board[move.from_row][move.from_col]:
-                    valid = True
+                    return True
                 if board[move.to_row][move.to_col - 1] == board[move.from_row][move.from_col]:
-                    valid = True
+                    return True
         if move.to_row - 1 > -1:
             if board[move.to_row - 1][move.to_col] == board[move.from_row][move.from_col]:
-                valid = True
+                return True
             if move.to_col + 1 < 7:
                 if board[move.to_row - 1][move.to_col + 1] == board[move.from_row][move.from_col]:
-                    valid = True
+                    return True
             if move.to_col - 1 > -1:
                 if board[move.to_row - 1][move.to_col - 1] == board[move.from_row][move.from_col]:
-                    valid = True
-        return valid
+                    return True
